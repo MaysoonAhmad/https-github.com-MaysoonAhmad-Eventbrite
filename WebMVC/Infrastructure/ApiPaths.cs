2a0ef7351e1040c8249d301b45146f08dd0e6126
MaysoonAhmad <31833085+MaysoonAhmad@users.noreply.github.com>
2020-04-30 12:12:17 -0700

    adding order Api

--- WebMVC/Infrastructure/ApiPaths.cs
+++ WebMVC/Infrastructure/ApiPaths.cs
@@ -1,94 +1,163 @@
 ﻿using System;
-
 using System.Collections.Generic;
-
 using System.Linq;
-
 using System.Threading.Tasks;
 
-
-
 namespace WebMVC.Infrastructure
-
 {
-
     public class ApiPaths
-
     {
-
-        public static class Catalog
-
+        public static class Basket
         {
+            public static string GetBasket(string baseUri, string basketId)
+            {
+                return $"{baseUri}/{basketId}";
+            }
 
+            public static string UpdateBasket(string baseUri)
+            {
+                return baseUri;
+            }
+
+            public static string CleanBasket(string baseUri, string basketId)
+            {
+                return $"{baseUri}/{basketId}";
+            }
+        }
+        public static class Catalog
+        {
             public static string GetAllCatalogItems(string baseUri,
 
-                int page, int take ,int? category, int? type, int? location)
 
-                               
+
+                 int page, int take, int? category, int? type, int? location)
+
+
+
+
+
             {
+
                 var filterQs = string.Empty;
+
                 if (category.HasValue || type.HasValue || location.HasValue)
+
+
 
                 {
 
+
+
                     var categoryQs = (category.HasValue) ? category.Value.ToString() : "null";
+
+
 
                     var typeQs = (type.HasValue) ? type.Value.ToString() : "null";
 
+
+
                     var locationQs = (location.HasValue) ? location.Value.ToString() : "null";
 
+
+
                     filterQs = $"/category/{categoryQs}/type/{typeQs}/location/{locationQs}";
+
+
 
                 }
 
 
 
+
+
+
+
                 return $"{baseUri}items{filterQs}?pageIndex={page}&pageSize={take}";
+
+
 
             }
 
-          
+
+
+
+
+
+
 
 
             public static string GetAllEventCategories(string baseUri)
 
+
+
             {
+
+
 
                 return $"{baseUri}catalogEventCategories";
 
+
+
             }
+
+
+
+
 
 
 
             public static string GetAllEventTypes(string baseUri)
 
+
+
             {
+
+
 
                 return $"{baseUri}catalogEventTypes";
 
+
+
             }
+
+
 
             public static string GetAllEventLocations(string baseUri)
 
+
+
             {
+
+
 
                 return $"{baseUri}catalogEventLocations";
 
+
+
+            }
+        }
+        public static class Order
+        {
+            public static string GetOrder(string baseUri, string orderId)
+            {
+                return $"{baseUri}/{orderId}";
+            }
+
+            //public static string GetOrdersByUser(string baseUri, string userName)
+            //{
+            //    return $"{baseUri}/userOrders?userName={userName}";
+            //}
+            public static string GetOrders(string baseUri)
+            {
+                return baseUri;
+            }
+            public static string AddNewOrder(string baseUri)
+            {
+                return $"{baseUri}/new";
             }
 
         }
-    }
-
-
-
-        public static class Basket
-
-        {
-
-
-
-        }
 
     }
-
+}
 
--- WebMVC/Startup.cs
+++ WebMVC/Startup.cs
@@ -1,34 +1,65 @@
 using System;
+
 using System.Collections.Generic;
+
 using System.Linq;
+
 using System.Threading.Tasks;
+
 using Microsoft.AspNetCore.Authentication.Cookies;
+
 using Microsoft.AspNetCore.Authentication.OpenIdConnect;
+
 using Microsoft.AspNetCore.Builder;
+
 using Microsoft.AspNetCore.Hosting;
+
 using Microsoft.AspNetCore.Http;
+
 using Microsoft.AspNetCore.HttpsPolicy;
+
 using Microsoft.Extensions.Configuration;
+
 using Microsoft.Extensions.DependencyInjection;
+
 using Microsoft.Extensions.Hosting;
+
 using Microsoft.IdentityModel.Tokens;
-using WebMVC.Infrastructure;
+
 using WebMVC.Services;
 
+using WebMVC.Infrastructure;
+using WebMVC.Models;
+using WebMvc.Services;
+
 namespace WebMVC
+
 {
+
     public class Startup
+
     {
+
         public Startup(IConfiguration configuration)
+
         {
+
             Configuration = configuration;
+
         }
+
+
 
         public IConfiguration Configuration { get; }
 
+
+
         // This method gets called by the runtime. Use this method to add services to the container.
+
         public void ConfigureServices(IServiceCollection services)
+
         {
+
             services.Configure<CookiePolicyOptions>(options =>
 
             {
@@ -40,9 +71,21 @@ namespace WebMVC
                 options.MinimumSameSitePolicy = SameSiteMode.None;
 
             });
+
             services.AddControllersWithViews();
+
             services.AddSingleton<IHttpClient, CustomHttpClient>();
+
             services.AddTransient<ICatalogService, CatalogService>();
+
+            services.AddSingleton<IHttpContextAccessor, HttpContextAccessor>();
+
+            services.AddTransient<IIdentityService<ApplicationUser>, IdentityService>();
+
+            services.AddTransient<ICartService, CartService>();
+
+            services.AddTransient<IOrderService, OrderService>();
+
             var identityUrl = Configuration.GetValue<string>("IdentityUrl");
 
             var callBackUrl = Configuration.GetValue<string>("CallBackUrl");
@@ -89,6 +132,10 @@ namespace WebMVC
 
                 options.Scope.Add("offline_access");
 
+                options.Scope.Add("basket");
+
+                options.Scope.Add("order");
+
                 options.TokenValidationParameters = new TokenValidationParameters()
 
                 {
@@ -114,32 +161,63 @@ namespace WebMVC
 
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+
         {
+
             if (env.IsDevelopment())
+
             {
+
                 app.UseDeveloperExceptionPage();
+
             }
+
             else
+
             {
+
                 app.UseExceptionHandler("/Home/Error");
+
                 // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+
                 app.UseHsts();
+
             }
+
             app.UseHttpsRedirection();
+
             app.UseStaticFiles();
+
+
 
             app.UseRouting();
 
-            app.UseAuthorization();
+
+
+            app.UseAuthentication();
+
             app.UseCookiePolicy();
 
+            app.UseAuthorization();
+
+
+
             app.UseEndpoints(endpoints =>
+
             {
+
                 endpoints.MapControllerRoute(
+
                     name: "default",
+
                     pattern: "{controller=Home}/{action=Index}/{id?}");
+
             });
+
         }
+
     }
+
 }
